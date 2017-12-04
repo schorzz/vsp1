04c29c329dcc29f66fec477f0708c5cda48517e1
= <>
2017-12-05 00:58:25 +0100

    dfgh

---
+++ src/main/java/QuestDo.java
@@ -0,0 +1,15 @@
+import dto.BroadcastInfo;
+
+public class QuestDo {
+    private String pfad ="/blackboard/quests";
+    private BroadcastInfo bci = null;
+
+
+    public QuestDo(BroadcastInfo bci) {
+        this.bci = bci;
+    }
+
+    public void doQuest(){
+        //Resty r = new Resty();
+    }
+}
